a047c7d7a0a8955e0b462033d3934688783325aa
Vexify4103 <painmain.shadow@gmail.com>
2025-07-18 12:16:35 +0200

    Update homepage features and install commands
    Refactored homepage feature descriptions and improved install command tabs to include yarn, pnpm, bun, and deno. Updated homepage text for clarity and consistency. Bumped package version to 2.8.7.

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,71 +4,71 @@
  * This source code is licensed under the MIT license found in the
  * THIRD_PARTY_LICENSE file in the `website` directory of this source tree.
  */
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Feature from '../Feature/Feature';
 import StatisticsFeature from '../Feature/StatisticsFeature';
 import styles from './styles.module.css';
-import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
-import Tippy from "@tippyjs/react";
-import clsx from "clsx";
-import {ClipboardIcon} from "@heroicons/react/24/outline";
-import Tabs from "@theme/Tabs";
-import TabItem from "@theme/TabItem";
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Tippy from '@tippyjs/react';
+import clsx from 'clsx';
+import { ClipboardIcon } from '@heroicons/react/24/outline';
+import Tabs from '@theme/Tabs';
+import TabItem from '@theme/TabItem';
 
 async function handleClickInstallButton(command) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
-    await window.navigator.clipboard.writeText(command);
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+	await window.navigator.clipboard.writeText(command);
 }
 
 const InstallTabButton = ({ installCommand, handleClickInstallButton }) => {
-    const [showTippy, setShowTippy] = useState(false);
+	const [showTippy, setShowTippy] = useState(false);
 
-    const toggleTippy = () => {
-        setShowTippy(true);
+	const toggleTippy = () => {
+		setShowTippy(true);
 
-        setTimeout(() => {
-            setShowTippy(false);
-        }, 1000);
-    };
+		setTimeout(() => {
+			setShowTippy(false);
+		}, 1000);
+	};
 
-    return (
-        <Tippy arrow content="Copied!" placement="auto" theme="discord" visible={showTippy}>
-            <button
-                className={clsx('button', 'button--secondary', 'button--lg', styles.button)}
-                onClick={() => {
-                    handleClickInstallButton();
-                    toggleTippy();
-                }}
-                type="button"
-            >
-                <div className={clsx(styles.buttonContent)}>
-                    {installCommand}
-                    <ClipboardIcon className={styles.copyIcon} />
-                </div>
-            </button>
-        </Tippy>
-    );
+	return (
+		<Tippy arrow content="Copied!" placement="auto" theme="discord" visible={showTippy}>
+			<button
+				className={clsx('button', 'button--secondary', 'button--lg', styles.button)}
+				onClick={() => {
+					handleClickInstallButton();
+					toggleTippy();
+				}}
+				type="button"
+			>
+				<div className={clsx(styles.buttonContent)}>
+					{installCommand}
+					<ClipboardIcon className={styles.copyIcon} />
+				</div>
+			</button>
+		</Tippy>
+	);
 };
 
 const InstallTabs = () => {
-    const npmInstallCommand = 'npm install magmastream';
-    // const yarnInstallCommand = npmToYarn(npmInstallCommand);
-    // const pnpmInstallCommand = npmToPnpm(npmInstallCommand);
-    // const bunInstallCommand = npmToBun(npmInstallCommand);
-    // const denoInstallCommand = 'deno install npm:magmastream';
+	const npmInstallCommand = 'npm install magmastream';
+	const yarnInstallCommand = 'yarn add magmastream';
+	const pnpmInstallCommand = 'pnpm add magmastream';
+	const bunInstallCommand = 'bun add magmastream';
+	const denoInstallCommand = 'deno install magmastream';
 
-    return (
-        <div className={styles.buttons}>
-            <Tabs className={styles.tabs} groupId="npm2yarn2pnpm">
-                <TabItem default label="npm" value="npm">
-                    <InstallTabButton
-                        handleClickInstallButton={() => {
-                            handleClickInstallButton(npmInstallCommand);
-                        }}
-                        installCommand={npmInstallCommand}
-                    />
-                </TabItem>
-                {/*				<TabItem label="yarn" value="yarn">
+	return (
+		<div className={styles.buttons}>
+			<Tabs className={styles.tabs} groupId="npm2yarn2pnpm">
+				<TabItem default label="npm" value="npm">
+					<InstallTabButton
+						handleClickInstallButton={() => {
+							handleClickInstallButton(npmInstallCommand);
+						}}
+						installCommand={npmInstallCommand}
+					/>
+				</TabItem>
+				<TabItem label="yarn" value="yarn">
 					<InstallTabButton
 						handleClickInstallButton={() => {
 							handleClickInstallButton(yarnInstallCommand);
@@ -99,42 +99,49 @@ const InstallTabs = () => {
 						}}
 						installCommand={denoInstallCommand}
 					/>
-				</TabItem>*/}
-            </Tabs>
-        </div>
-
-    );
+				</TabItem>
+			</Tabs>
+		</div>
+	);
 };
 
 const HomePageFeatures = ({ siteConfig }) => (
-    <section className={styles.features}>
-        <div className="container">
-            <div className="row">
-                <div className={clsx('col', 'col--4', styles.feature)}>
-                    <div className="text--left padding-horiz--md">
-                        <h3>About & features</h3>
-                        <div>
-                            {siteConfig.tagline}
-                            <br />
-                            <i className="fa-solid fa-check" style={{
-                                color: '#b85b13',
-                                marginRight: '0.4rem'
-                            }}></i><a href={"/docs/enums/SearchPlatform"}>Multiple Sources</a> Powered by <a href={"https://github.com/topi314/LavaSrc"}>LavaSrc</a>
-                            <br />
-                            <i className="fa-solid fa-check" style={{
-                                color: '#b85b13',
-                                marginRight: '0.4rem'
-                            }}></i>Unleash Your Music Across All Major Platforms, From SoundCloud to Spotify and Beyond!
-                        </div>
-                    </div>
-                </div>
-                <div className={clsx('col', 'col--5', styles.feature)}>
-                   <InstallTabs />
-                </div>
-                <StatisticsFeature />
-            </div>
-        </div>
-    </section>
+	<section className={styles.features}>
+		<div className="container">
+			<div className="row">
+				<div className={clsx('col', 'col--4', styles.feature)}>
+					<div className="text--left padding-horiz--md">
+						<h3>About & features</h3>
+						<div>
+							{siteConfig.tagline}
+							<br />
+							<i
+								className="fa-solid fa-check"
+								style={{
+									color: '#b85b13',
+									marginRight: '0.4rem',
+								}}
+							></i>
+							<a href={'/docs/enums/SearchPlatform'}>Multiple Sources</a> Powered by <a href={'https://github.com/topi314/LavaSrc'}>LavaSrc</a>
+							<br />
+							<i
+								className="fa-solid fa-check"
+								style={{
+									color: '#b85b13',
+									marginRight: '0.4rem',
+								}}
+							></i>
+							Power up your Discord bot with Lavalink - stream audio from anywhere!
+						</div>
+					</div>
+				</div>
+				<div className={clsx('col', 'col--5', styles.feature)}>
+					<InstallTabs />
+				</div>
+				<StatisticsFeature />
+			</div>
+		</div>
+	</section>
 );
 
 export default HomePageFeatures;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,65 +5,58 @@ import Layout from '@theme/Layout';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import styles from './index.module.css';
-import React from "react";
+import React from 'react';
 
 function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-          <img
-            src="/img/logo.ico"
-            height="250"
-          />
-          <h1 className="fire-header_nye2">
-              {siteConfig.title}
-          </h1>
-      </div>
-    </header>
-  );
+	const { siteConfig } = useDocusaurusContext();
+	return (
+		<header className={clsx('hero hero--primary', styles.heroBanner)}>
+			<div className="container">
+				<img src="/img/logo.ico" height="250" />
+				<h1 className="fire-header_nye2">{siteConfig.title}</h1>
+			</div>
+		</header>
+	);
 }
 
 export default function Home() {
-    const {siteConfig} = useDocusaurusContext();
+	const { siteConfig } = useDocusaurusContext();
 
-  return (
-    <Layout
-      title={`🔥 Homepage`}
-    >
-      <HomepageHeader />
-      <main>
-          <div className={"container"}>
-              <p className={styles.p}>
-                  Embark on an Epic Musical Journey: Explore the Boundless Potential of Magmastream! Immerse Yourself in Seamless Streaming Across Deezer, Spotify, SoundCloud, and Beyond.
-              </p>
-              <div className={styles.buttons}>
-                  <Link
-                      className="button button--secondary button--lg"
-                      style={{
-                          margin: '0 1rem',
-                          color: "#fff",
-                          backgroundColor: "#ff6a00",
-                          borderColor: "#ff6a00"
-                      }}
-                      to="/docs/intro">
-                      Get Started
-                  </Link>
-                  <Link
-                      className="button button--secondary button--lg"
-                      style={{
-                          margin: '0 1rem',
-                          color: "#fff",
-                          backgroundColor: "transparent",
-                          borderColor: "transparent"
-                      }}
-                      to="https://github.com/Magmastream-NPM">
-                      Github <i className="fa-solid fa-arrow-right-from-bracket fa-xs"></i>
-                  </Link>
-              </div>
-              <HomepageFeatures siteConfig={siteConfig} />
-          </div>
-      </main>
-    </Layout>
-  );
+	return (
+		<Layout title={`🔥 Homepage`}>
+			<HomepageHeader />
+			<main>
+				<div className={'container'}>
+					<p className={styles.p}>A powerful Lavalink / NodeLink client for streaming music from Spotify, Deezer, SoundCloud, and more.</p>
+					<div className={styles.buttons}>
+						<Link
+							className="button button--secondary button--lg"
+							style={{
+								margin: '0 1rem',
+								color: '#fff',
+								backgroundColor: '#ff6a00',
+								borderColor: '#ff6a00',
+							}}
+							to="/docs/intro"
+						>
+							Get Started
+						</Link>
+						<Link
+							className="button button--secondary button--lg"
+							style={{
+								margin: '0 1rem',
+								color: '#fff',
+								backgroundColor: 'transparent',
+								borderColor: 'transparent',
+							}}
+							to="https://github.com/Magmastream-NPM"
+						>
+							Github <i className="fa-solid fa-arrow-right-from-bracket fa-xs"></i>
+						</Link>
+					</div>
+					<HomepageFeatures siteConfig={siteConfig} />
+				</div>
+			</main>
+		</Layout>
+	);
 }
